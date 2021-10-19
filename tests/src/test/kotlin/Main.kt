84a2a84c1083a4e15afb7a0880bb5369c4981f32
Serghei Novicov <telimas66@gmail.com>
2021-10-19 21:45:47 +0300

    Added assembly plugin for tests module

--- tests/src/test/kotlin/Main.kt
+++ tests/src/test/kotlin/Main.kt
@@ -4,6 +4,7 @@ import org.junit.runner.JUnitCore
 import org.junit.runner.Request
 import kotlin.system.exitProcess
 
+// TODO: Leave comments
 fun main(args: Array<String>) {
     if (System.getProperty("task2").isNullOrEmpty())
         JUnitCore.main(RunCucumberTests::class.java.canonicalName, *args)
