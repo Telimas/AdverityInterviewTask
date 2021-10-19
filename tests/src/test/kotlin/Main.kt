3c2bffa757149497b56323528befa40b7008d70b
Serghei Novicov <telimas66@gmail.com>
2021-10-19 23:27:59 +0300

    Added javadoc & comments in Main.kt

--- tests/src/test/kotlin/Main.kt
+++ tests/src/test/kotlin/Main.kt
@@ -4,11 +4,25 @@ import org.junit.runner.JUnitCore
 import org.junit.runner.Request
 import kotlin.system.exitProcess
 
-// TODO: Leave comments
+/**
+ * Main method was created for jar/maven run.
+ * You can ignore this method and run tests directly from:
+ * @see RunCucumberTests
+ * @see HydroMeteorologicChecker
+ */
 fun main(args: Array<String>) {
+    /*
+    Checking for [task2] system property just not to start 2 separate test classes and the same time.
+     */
     if (System.getProperty("task2").isNullOrEmpty())
+    /*
+    Starting Cucumber tests.
+     */
         JUnitCore.main(RunCucumberTests::class.java.canonicalName, *args)
     else {
+        /*
+        Starting Service automation.
+         */
         val req = Request.method(HydroMeteorologicChecker::class.java, "checkWarnings")
         val res = JUnitCore().also { it.addListener(TextListener(RealSystem())) }.run(req)
         exitProcess(if (res.wasSuccessful()) 0 else 1)
