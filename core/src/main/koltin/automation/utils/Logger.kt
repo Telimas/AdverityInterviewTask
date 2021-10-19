cde02770bd75a5091e26d6ca475cee7b2d8e2ce1
Serghei Novicov <telimas66@gmail.com>
2021-10-19 18:22:20 +0300

    Temporally removed logger method with class argument

--- core/src/main/koltin/automation/utils/Logger.kt
+++ core/src/main/koltin/automation/utils/Logger.kt
@@ -4,9 +4,5 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 fun <T : Any> T.logger(): Logger {
-    return logger(this.javaClass)
-}
-
-private fun <T : Any> logger(forClass: Class<T>): Logger {
-    return LoggerFactory.getLogger(forClass)
+    return LoggerFactory.getLogger(this.javaClass)
 }
